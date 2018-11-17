327b3b9d28483ffb8d49ed2d50448c57ddc28744
youknowimcomingwhenyouhearmehumming <31764934+youknowimcomingwhenyouhearmehumming@users.noreply.github.com>
2018-11-17 19:53:35 +0100

    Changed to matlab

---
+++ subMatrix.m
@@ -0,0 +1,19 @@
+function [subMatrix, offsetH, offsetW] = subMatrix(matrix,pointX,pointY,subMatrixW,subMatrixH)
+%SUBMATRIX Summary of this function goes here. Author=Rasmus. Prime goal is
+%to cut out a submatrix of the image that is inserted in the function. THis
+%cut out will be centered around the x,y coordinated that ´is put into the
+%function. 
+%   Detailed explanation goes here
+
+subMatrix = zeros(subMatrixH,subMatrixW);
+yfrom = (pointY-floor(subMatrixH/2));
+yto = (pointY+ceil(subMatrixH/2));
+xfrom = (pointX-floor(subMatrixW/2));
+xto = (pointX+ceil(subMatrixW/2));
+
+subMatrix = matrix(yfrom:yto,xfrom:xto);
+
+offsetH = pointY-floor(subMatrixH/2);
+offsetW = pointX-floor(subMatrixW/2);
+end
+
